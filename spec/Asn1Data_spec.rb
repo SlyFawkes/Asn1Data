235e30c4d9ce46a73d6a68922499cb74d68f4881
dan <deanwelch286@gmail.com>
2022-06-19 22:06:44 +0100

    Initial Commit

---
+++ spec/Asn1Data_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Asn1Data do
+  it "has a version number" do
+    expect(Asn1Data::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
